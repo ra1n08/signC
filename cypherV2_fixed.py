d7cbf55eec0c6f81692c975bf0516c3bfe27d7dd
t_hai <azttt2008@gmail.com>
2023-04-28 06:18:31 +0000

    new cypher fixed

---
+++ cypherV2_fixed.py
@@ -0,0 +1,138 @@
+import secrets
+from _math import is_prime
+import ast
+import random
+
+class ParamGenerater:
+    @staticmethod
+    def generate_prime(bit):
+        Q = random.getrandbits(bit)
+        while not is_prime(Q):
+            Q = random.getrandbits(bit)
+        return Q
+    
+    def run(self, bit):
+        # generate Q
+        Q = ParamGenerater.generate_prime(bit)
+        # generate R
+        R = random.getrandbits(20)
+        
+        while True:
+            # calculate P
+            P = Q * R + 1
+            if is_prime(P):
+                print("P=", P)
+                with open(f"./thamso/P.txt", "w") as f:
+                    f.write(str(P))
+                break
+            else:
+                Q = ParamGenerater.generate_prime(bit)
+                R = random.getrandbits(20)
+        
+        print("Q=", Q)
+        print("R=", R)
+        
+        with open(f"./thamso/P.txt", "r") as f:
+            p = int(f.read().strip())
+            
+        # generate Q value
+        Q = ParamGenerater.generate_prime(bit)
+        
+        PR_path = f"./thamso/P_R.txt"
+        with open(PR_path, "w") as f:
+            # iterate over values of R from 1 to 100
+            for R in range(1, 100000):
+                # calculate the value of P
+                P = Q * R + 1
+                # check if P is prime
+                if is_prime(P):
+                    f.write(f"({P}, {R})\n")
+        print("generated P and R")
+        
+        with open(PR_path, "r") as f:
+            pairs = [ast.literal_eval(line.strip()) for line in f]
+        
+        valid_g = False
+        while not valid_g:
+            # select random pairs
+            P, R = random.choice(pairs)
+            # choose a random value h fro, [(1, P)]
+            h = random.randint(1, P-1)
+            
+            # compute g = h^R !=1 mod P
+            g = pow(h, R, P)
+            # check that g^R != 1 mod P
+            if pow(g, R, P) != 1:
+                valid_g = True
+        
+        with open(f"./thamso/tapG.txt", "w") as f:
+            f.write(f"{g}\n")
+        
+        x = g
+        with open(f"./thamso/giatriX.txt", "w") as f:
+            f.write(f"{x}\n")
+        print("X=", x)
+        print("X saved successfully!")
+        
+        # choose random A then print out and save to a.txt
+        a = random.randint(2, 200)
+        print("a=", a)
+        with open(f"./thamso/a.txt", "w") as f:
+            f.write(str(a))
+        
+        # with open(PR_path, "r") as f:
+        #     pairs = [ast.literal_eval(l.strip()) for l in f]
+            
+        valid_xa = False
+        while not valid_xa:
+            # select random pairs (P, R)
+            P, R = random.choice(pairs)
+            # choose a random value h from [(1, P)]
+            h = random.randint(1, P-1)
+            # compute g = h^R mod P
+            xa = pow(h, R, P)
+            # check that g^R !=1 < g
+            if pow(xa, R, P) != 1 and xa < g:
+                valid_xa = True
+        
+        with open(f"./thamso/Xa.txt", "w") as f:
+            f.write(f"{xa}\n")
+        with open(f"./thamso/Xa.txt", "w") as f:
+            f.write(str(xa))
+        # print(f"x_a key {xa} saved successfully")
+        # calculate public key of sender then write to ya.txt
+        y_a = pow(a, xa, p)
+        with open(f"./thamso/Ya.txt", "w") as f:
+            f.write(str(y_a))
+        print(f"x_a key {xa} and y_a key {y_a} saved successfully")
+        
+        # with open(PR_path, "r") as f:
+        #     pairs = [ast.literal_eval(l.strip()) for l in f]
+        
+        valid_xb = False
+        while not valid_xb:
+            # select a random pair (P, R)
+            P, R = random.choice(pairs)
+            # choose a random value h from [(1, P)]
+            h = random.randint(1, P-1)
+            # Compute g = h^R mod P
+            xb = pow(h, R, P)
+            # Check that g^R != 1 mod P
+            if pow(xb, R, P) != 1 and xb !=g and xb != x:
+                valid_xb = True
+        
+        with open(f"./thamso/Xb.txt", "w") as f:
+            f.write(f"{xb}\n")
+        with open(f"./thamso/Xh.txt", "w") as f:
+            f.write(str(xb))
+        
+        y_b = pow(a, xb ,p)
+        with open(f"./thamso/Yb.txt", "w") as f:
+            f.write(str(y_b))
+        print(f"x_b key {xb} and y_b key {y_b} saved successfully")
+        
+        iv = secrets.token_bytes(16)
+        with open(f"./thamso/iv.txt", "wb") as f:
+            f.write(iv)
+        print("generated parameters!")
+        
--- run.py
+++ run.py
@@ -1,46 +1,20 @@
-from cypher_v2 import Parameter, Sign, unSign
-import tqdm
+import os
+try: import tqdm
+except:
+    os.system(f"python3 -m pip install -r requirements.txt")
+
+from cypherV2_fixed import ParamGenerater
 import time
 import sys
 
+import argparse
 
-class main:
-    def __init__(self) -> None:
-        self.param = Parameter(bit=160)
-        self.sign = Sign()
-        self.unSign = unSign()
-
-    def run_param(self):
-        # self.per.bit(160)
-        self.param.write_P()
-        self.param.read_P()
-        print("P = ", self.param.P)
-        print("Q = ", self.param.Q)
-        print("R = ", self.param.R)
-        for percent in self.param.generate_matching_pair():
-            print(f"Đang tạo (P, R) {percent:.2f}%", end="\r")
-        print("Tạo (P,R) thành công")
-        self.param.select_g()
-        self.param.select_a()
-        self.param.sender_XA()
-        self.param.sender_XB()
-        self.param.random_chain()
-        print("Tạp tham số thành công, yey :33")
-
-    def run_sign(self, inp):
-        self.sign.create_keys()
-        print("Tạo khóa k1, k2 thành công!")
-        self.sign.encrypt(inp=inp)
-        print("Mã hóa thành công!")
-        self.sign.calculate_signature_r(input_file=inp)
-        print("Tính chữ kí r thành công!")
-        self.sign.calculate_s()
-        print("Tính s thành công!")
-
-
+app = ParamGenerater()
+    
 if __name__ == "__main__":
-    app = main()
-    runtime = time.time()
-    app.run_param()
-    app.run_sign(f"./input/input.txt")
-    print("Thời gian thực hiện: {:.2f} seconds".format(time.time() - runtime))
+    parser = argparse.ArgumentParser(description="signing a text file!")
+    parser.add_argument('-b', '--bit', type=int, help="bit to generate Q in Param", default="64")
+    args = parser.parse_args()
+    
+    app.run(args.bit)
+
