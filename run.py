46710379b74917c4984b47abf56eece0905e289b
t_hai <azttt2008@gmail.com>
2023-04-29 16:50:41 +0000

    fixed signCrypt classs ad fixed CLI

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-import os
-try: import tqdm
-except:
-    os.system(f"python3 -m pip install -r requirements.txt")
+# import os
+# try: import tqdm
+# except:
+#     os.system(f"python3 -m pip install -r requirements.txt")
 
 from cypherV2_fixed import ParamGenerater, Sign
 import time
@@ -27,22 +27,23 @@ if __name__ == "__main__":
     print(banner)
     parser = argparse.ArgumentParser(description="idk write what in here :)))")
     # parser.add_argument("mode", choices=["cp", "sign", "unsign"], help="cp is Create Param, for generate parameters, \n sign for start SignCrypt, \n unsign for start Un-SignCrypt (DeCrypt)")
-    
+    commands = parser.add_subparsers(dest="command")
     # tạo group cho lệnh tạo tham số
-    cp_parser = parser.add_argument_group("cp")
-    cp_parser.add_argument('-b', '--bit', dest="bit", type=int, help="bit to generate Q in Param (default: 64)", default="64")
+    cp_parser = commands.add_parser("cp", help="generate keys")
+    cp_parser.add_argument('-b', '--bit', dest="bit", type=int, help="bit to generate Q in Param (default: 64)", default=64)
     
     # tạo group cho lệnh sign
-    sign_parser = parser.add_argument_group("sign")
+    sign_parser = commands.add_parser("sign", help="signCrypt file")
     sign_parser.add_argument('-i', '--input', dest="sign_in", type=str, help="input file for sign")
     sign_parser.add_argument('-o', '--output', dest="sign_out", type=str, help="output PATH for signed file and r,s signature (default: ./c.r.s/)", default="./c.r.s")
-    
-    # tạo group cho lệnh unSign
-    unsign_parser = parser.add_argument_group("unsign")
-    unsign_parser.add_argument('-i', '--input', dest="unsign_in", type=str, help="input FILE for unSignCryption")
-    unsign_parser.add_argument('-o', '--output', dest="unsign_out", type=str, help="output PATH for unSignCryption")
+
+    # # tạo group cho lệnh unSign
+    # unsign_parser = parser.add_argument_group("unsign")
+    # unsign_parser.add_argument('-i', '--input', dest="unsign_in", type=str, help="input FILE for unSignCryption")
+    # unsign_parser.add_argument('-o', '--output', dest="unsign_out", type=str, help="output PATH for unSignCryption")
     args = parser.parse_args()
     
+    print(args)
     if args.command == "cp":
         print("now at generate parameter mode!")
         cp.run(args.bit)
@@ -52,5 +53,5 @@ if __name__ == "__main__":
     if args.command == "unsign":
         print("now at unSign mode!")
         # unsign.run()
-        print(args.unsign_in, args.unsign_out)
+        print(args.unsign_in, args.unsign_out) # call duoc khong alo
 
